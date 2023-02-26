9e49f4e465a6bf156b785fa55f205abdb1a0fa51
z3y <33181641+z3y@users.noreply.github.com>
2023-02-26 15:31:47 +0100

    add blue noise dither tex

--- Editor/Importer/LitImporter.cs
+++ Editor/Importer/LitImporter.cs
@@ -52,7 +52,7 @@ namespace z3y.Shaders
             var code = ProcessFileLines(settings, ctx.assetPath, ctx.selectedBuildTarget);
             var shader = ShaderUtil.CreateShaderAsset(code, false);
 
-            EditorMaterialUtility.SetShaderNonModifiableDefaults(shader, new[] { "_DFG" }, new Texture[] { DFGLut() });
+            EditorMaterialUtility.SetShaderNonModifiableDefaults(shader, new[] { "_DFG", "BlueNoiseCrossFade" }, new Texture[] { DFGLut(), BlueNoiseCrossFade() });
             
             ctx.DependsOnSourceAsset("Assets/com.z3y.shaders/Editor/Importer/LitImporter.cs");
 
@@ -426,8 +426,14 @@ namespace z3y.Shaders
 
         public static Texture2D DFGLut()
         {
-            const string DFGPATH = "Packages/com.z3y.shaders/ShaderLibrary/dfg-multiscatter.exr"; //TODO: replace the package path with const string
-            return AssetDatabase.LoadAssetAtPath<Texture2D>(DFGPATH);
+            const string path = "Packages/com.z3y.shaders/ShaderLibrary/dfg-multiscatter.exr"; //TODO: replace the package path with const string
+            return AssetDatabase.LoadAssetAtPath<Texture2D>(path);
         }
+        public static Texture2D BlueNoiseCrossFade()
+        {
+            const string path = "Packages/com.z3y.shaders/ShaderLibrary/BlueNoiseCrossfade.png";
+            return AssetDatabase.LoadAssetAtPath<Texture2D>(path);
+        }
+
     }
 }
