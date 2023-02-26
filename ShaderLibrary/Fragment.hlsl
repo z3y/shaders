8c9c9af529d8259a1f2dca99c5bd64f647772dff
z3y <33181641+z3y@users.noreply.github.com>
2023-02-26 16:09:20 +0100

    add lod crossfade

--- ShaderLibrary/Fragment.hlsl
+++ ShaderLibrary/Fragment.hlsl
@@ -5,6 +5,10 @@ half4 frag (Varyings input) : SV_Target
     UNITY_SETUP_INSTANCE_ID(input);
     UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
 
+    #ifdef LOD_FADE_CROSSFADE
+    UNITY_APPLY_DITHER_CROSSFADE(input.positionCS);
+    #endif
+
     SurfaceDescription surfaceDescription = InitializeSurfaceDescription();
     #ifdef USE_SURFACEDESCRIPTION
     SurfaceDescriptionFunction(input, surfaceDescription);
