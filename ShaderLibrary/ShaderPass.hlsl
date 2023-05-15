114a0e438e72daf2ce11c063f08147414c11a717
z3y <33181641+z3y@users.noreply.github.com>
2023-05-15 21:36:02 +0200

    include CommonMaterial by default

--- ShaderLibrary/ShaderPass.hlsl
+++ ShaderLibrary/ShaderPass.hlsl
@@ -138,6 +138,7 @@ bool IsInMirror()
     #include "Packages/com.z3y.shaders/ShaderLibrary/CoreRP/Packing.hlsl"
     // #include "Packages/com.z3y.shaders/ShaderLibrary/CoreRP/EntityLighting.hlsl"
     #include "Packages/com.z3y.shaders/ShaderLibrary/CoreRP/Texture.hlsl"
+    #include "Packages/com.z3y.shaders/ShaderLibrary/CoreRP/CommonMaterial.hlsl"
 #endif
 
 
