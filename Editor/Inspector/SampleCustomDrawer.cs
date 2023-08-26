421b7753c8fb0a31f12d5be49e56ce95a37a3711
z3y <33181641+z3y@users.noreply.github.com>
2023-08-26 19:12:47 +0200

    custom attribute drawer

---
+++ Editor/Inspector/SampleCustomDrawer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace z3y.Shaders
+{
+    // Usage: [CustomDrawer(SampleCustomDrawer)] 
+    public class SampleCustomDrawer : ICustomPropertyDrawer
+    {
+
+        public void OnInitializeEditor(DefaultInspector.Property property, MaterialEditor editor, MaterialProperty[] materialProperties)
+        {
+            // executes once before the first frame
+            var attributes = property.attributes;
+        }
+
+        public void OnInspectorGUI(DefaultInspector.Property property, MaterialEditor editor, MaterialProperty[] materialProperties)
+        {
+            // executes every frame
+            EditorGUILayout.LabelField("This is a SampleCustomDrawer");
+            var materialProperty = materialProperties[property.index];
+            editor.ShaderProperty(materialProperty, property.displayName);
+        }
+    }
+}
