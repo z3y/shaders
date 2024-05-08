b24fe8a8d0ddd7d294ce201e0a9be9c79fba02a7
z3y <z3y@protonmail.com>
2024-05-08 19:52:33 +0200

    migrate from standard script

--- Editor/StandardMigration.cs
+++ Editor/StandardMigration.cs
@@ -1,6 +1,8 @@
+using System.IO;
 using UnityEditor;
 using UnityEngine;
-
+using UnityEngine.Experimental.Rendering;
+using static z3y.FreeImagePacking;
 namespace z3y
 {
     public static class StandardMigration
@@ -8,10 +10,20 @@ namespace z3y
         [MenuItem("Tools/Lit/Migrate Material Selection")]
         public static void MigrateSelection()
         {
-            foreach (Material obj in Selection.GetFiltered(typeof(Material), SelectionMode.Assets))
+            var objs = Selection.GetFiltered(typeof(Material), SelectionMode.Assets);
+            var len = objs.Length;
+            for (int i = 0; i < objs.Length; i++)
             {
+                var obj = objs[i] as Material;
+                if (obj == null)
+                {
+                    continue;
+                }
+                EditorUtility.DisplayProgressBar("Migrating", "Doing some work...", i / len);
                 Migrate(obj);
             }
+
+            EditorUtility.ClearProgressBar();
         }
 
         public static void Migrate(Material material)
@@ -27,7 +39,7 @@ namespace z3y
             }
 
             Undo.RecordObject(material, "Migrating Material");
-            Debug.Log($"Migrating: {material.name}");
+
 
             var smoothness = material.GetFloat("_Glossiness");
             var metallic = material.GetFloat("_Metallic");
@@ -37,6 +49,7 @@ namespace z3y
             var detailNormalScale = material.GetFloat("_DetailNormalMapScale");
             var detailNormal = material.GetTexture("_DetailNormalMap");
             var detailuv = material.GetFloat("_UVSec");
+            var occlusionMap = material.GetTexture("_OcclusionMap");
 
             var emission = material.GetColor("_EmissionColor");
 
@@ -44,16 +57,17 @@ namespace z3y
 
             material.shader = Shader.Find("Lit");
 
+
+            material.SetFloat("_Roughness", 1.0f - smoothness);
+
             if (metallicGlossMap)
             {
-                material.SetFloat("_Roughness", 1.0f);
+                material.SetFloat("_Metallic", 1.0f);
             }
             else
             {
-                material.SetFloat("_Roughness", 1.0f - smoothness);
+                material.SetFloat("_Metallic", Mathf.Pow(metallic, 2.2f));
             }
-
-            material.SetFloat("_Metallic", Mathf.Pow(metallic, 2.2f));
             material.SetColor("_EmissionColor", new Color(Mathf.Pow(emission.r, 2.2f), Mathf.Pow(emission.g, 2.2f), Mathf.Pow(emission.b, 2.2f)));
 
             if (emissionEnabled)
@@ -76,6 +90,71 @@ namespace z3y
                 material.EnableKeyword("_DETAIL_ALBEDO");
             }
 
+            bool needsPacking = occlusionMap || metallicGlossMap;
+            if (needsPacking)
+            {
+                var refTex = metallicGlossMap == null ? occlusionMap : metallicGlossMap;
+
+
+                var path = AssetDatabase.GetAssetPath(refTex);
+                var fullPath = Path.GetFullPath(path);
+
+                var absolutePath = FreeImagePackingEditor.GetPackedTexturePath(fullPath);
+                var unityPath = FreeImagePackingEditor.GetPackedTexturePath(path);
+
+
+                // occlusion
+                var r = new TextureChannel();
+                r.DefaultColor = DefaultColor.White;
+                r.Source = ChannelSource.Green;
+                if (occlusionMap)
+                {
+                    r.Path = AssetDatabase.GetAssetPath(occlusionMap);
+                }
+
+                // roughness
+                var g = new TextureChannel();
+                g.DefaultColor = DefaultColor.White;
+                g.Source = ChannelSource.Alpha;
+
+                if (metallicGlossMap)
+                {
+                    var hasAlpha = GraphicsFormatUtility.HasAlphaChannel(metallicGlossMap.graphicsFormat);
+                    if (hasAlpha)
+                    {
+                        Debug.Log("true");
+                        g.Invert = true;
+                        g.Path = AssetDatabase.GetAssetPath(metallicGlossMap);
+                    }
+                }
+
+                // metallic
+                var b = new TextureChannel();
+                b.DefaultColor = DefaultColor.White;
+                b.Source = ChannelSource.Red;
+                if (metallicGlossMap)
+                {
+                    b.Path = AssetDatabase.GetAssetPath(metallicGlossMap);
+                }
+
+                var a = new TextureChannel();
+
+
+                PackCustom(absolutePath, r,g,b,a, (refTex.width, refTex.height), PackingFormat);
+                AssetDatabase.ImportAsset(unityPath);
+
+                // metallic gloss map alpha is always linear, but red and occlusion map can differ, if its not setup correctly
+                // this assumes it should be linear so srgb is turned off
+                var importer = AssetImporter.GetAtPath(unityPath);
+                (importer as TextureImporter).sRGBTexture = false;
+                importer.SaveAndReimport();
+
+                var maskMap = AssetDatabase.LoadAssetAtPath<Texture2D>(unityPath);
+
+                material.SetTexture("_MaskMap", maskMap);
+                material.EnableKeyword("_MASKMAP");
+
+            }
 
         }
 
